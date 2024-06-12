11a1c500ed933483ec3eee12f47d266f5a3ff517
alexeidoell <alexeidoell@gmail.com>
2024-06-11 21:38:45 -0600

    Initial implementation of 1 byte load instructions, excluding halt

--- core/core.cpp
+++ core/core.cpp
@@ -1,11 +1,11 @@
 #include <memory>
 #include <iostream>
-#include "lib/types.h"
+#include "../lib/types.h"
+#include "core.h"
 
 class Core {
 private:
-    u8 C = 0, B = 0, E = 0, D = 0, L = 0, H = 0, F = 0, A = 0;
-    u16 SP; u16 PC;
+    gbRegisters registers;
     std::array<u8, 16384> memory = { 0 };
     void op_tree();
 public:
@@ -16,16 +16,24 @@ public:
 };
 
 void Core::op_tree() {
-    u8 byte1 = memory[PC]; 
+    u8 byte1 = memory[registers.pc]; 
 
     if (byte1 == 0b01110110) { // halt op
+        // gotta implement halt
     }
     else if (byte1 >= 0x40 && byte1 < 0x80) { // load op
         u8 src = byte1 & 0b111;
         u8 dst = (byte1 >> 3) & 0b111;
 
+        // HL in src or dst
+        if (src == 6) {
+            u16 hl = (registers.gpr.n.h << 8) + registers.gpr.n.l;
+            registers.gpr.r[dst] = memory[hl];
+        } else if (dst == 6) {
+            u16 hl = (registers.gpr.n.h << 8) + registers.gpr.n.l;
+            memory[hl] = registers.gpr.r[src];
+        } else {
+            registers.gpr.r[dst] = registers.gpr.r[src];
+        }
     }
-
-
-
 }
