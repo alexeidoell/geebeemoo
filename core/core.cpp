2ea7e275f53612389712c28516a2b65113ba5daa
alexeidoell <alexeidoell@gmail.com>
2024-06-12 23:31:12 -0600

    fixed add instruction for [HL] as operand

--- core/core.cpp
+++ core/core.cpp
@@ -6,7 +6,7 @@ class Core {
 private:
     gbRegisters registers;
     std::array<u8, 16384> memory = { 0 };
-    void op_tree();
+    u8 op_tree();
 public:
     Core() {
         
@@ -14,8 +14,9 @@ public:
     ~Core();
 };
 
-void Core::op_tree() {
+u8 Core::op_tree() {
     u8 byte1 = memory[registers.pc]; 
+    u8 cycles = 4;
 
     if (byte1 == 0) { // nop
         // gotta implement nop
@@ -32,26 +33,42 @@ void Core::op_tree() {
             u16 hl = (registers.gpr.n.h << 8) + registers.gpr.n.l;
             registers.gpr.r[dst] = memory[hl];
         } else if (dst == 6) {
+            cycles = 8;
             u16 hl = (registers.gpr.n.h << 8) + registers.gpr.n.l;
             memory[hl] = registers.gpr.r[src];
         } else {
+            cycles = 8;
             registers.gpr.r[dst] = registers.gpr.r[src];
         }
     } else if (byte1 >= 0x80 && byte1 < 0xC0) { // arithmetic
+        u8 operand;
+        u8 operandValue;
+        operand = byte1 & 0b111;
+        if (operand != 6) operandValue = registers.gpr.r[operand];
+        else {
+            cycles = 8;
+            u16 hl = (registers.gpr.n.h << 8) + registers.gpr.n.l;
+            operandValue = memory[hl];
+        }
+        u16 result;
+
         if (byte1 < 0x88) { // addition
             registers.flags &=  0b11111101; // subtraction bit
-            u8 operand = byte1 & 0b111;
-
-            u16 result = registers.gpr.n.a + registers.gpr.r[operand];
+            result = registers.gpr.n.a + operandValue;
 
             if (result > 0xFF) registers.flags |= 0b00001000; // carry bit
             else registers.flags &= 0b11110111;
             if (result == 0) registers.flags |= 0b00000001; // zero bit
             else registers.flags &= 0b11111110;
-            if ((((registers.gpr.n.a & 0xF) + (registers.gpr.r[operand] & 0xF)) & 0x10) == 0x10) registers.flags |= 0b00000100; // half carry bit
+            if ((((registers.gpr.n.a & 0xF) + (operandValue & 0xF)) & 0x10) == 0x10) registers.flags |= 0b00000100; // half carry bit
             else registers.flags &= 0b11111011;
 
             registers.gpr.n.a = result & 0xFF;
         }
+        else if (byte1 < 0x90) { // conditional add
+
+
+        }
     }
+    return cycles;
 }
--- lib/types.h
+++ lib/types.h
@@ -5,6 +5,8 @@
 
 using s8 = int8_t;
 using s16 = int16_t;
+using s32 = int32_t;
 
 using u8 = uint8_t;
 using u16 = uint16_t;
+using u32 = uint32_t;
