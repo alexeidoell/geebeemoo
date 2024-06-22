c99cbd510faedd6de07e6170c0628b3e6fd970bf
alexeidoell <alexeidoell@gmail.com>
2024-06-22 17:12:51 -0600

    moved gb objetc off heap and onto stack

--- gb.h
+++ gb.h
@@ -8,9 +8,8 @@ class GB {
 public:
     void runEmu(char* filename);
     void doctor_log(std::ofstream& log, Core& core, MMU& mem);
-    GB() {
-    }
-    ~GB() {}
+    GB() = default;
+    ~GB() = default;
 
 };
 
--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include "gb.h"
 
 int main(int argc, char* argv[]) {
-    std::unique_ptr<GB> testGB = std::make_unique<GB>();
-    testGB->runEmu(argv[1]);
+//    std::unique_ptr<GB> testGB = std::make_unique<GB>();
+    GB testGB;
+    testGB.runEmu(argv[1]);
 
 
 }
